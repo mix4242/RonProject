ff7594340e3bfa62940cfbaef6889dea823d58af
Max Dudzinski <md3414@ray17.doc.ic.ac.uk>
2017-02-19 21:41:25 +0000

    changed to use vertices

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import csv
 import math
 import random
 
-from Dijkstra import Dijkst
+from Dijkstra import Dijkst, calcWei
 
-XI = 0.01
+XI = 0.10
 RANGEEX51 = range(58)
 RANGEEX51.remove(51)
 
@@ -42,37 +42,52 @@ def updateWij(RomeA, RomeB, wei, weiZeroth, cars):
         i = RomeA[n]-1
         j = RomeB[n]-1
         wei[i,j] = weiZeroth[i,j] + (XI * ((cars[i] + cars[j]) / 2 ) )
-        
+
+nextNode = np.zeros(shape=58)
 def RomeSimulate(RomeA, RomeB, wei, weiZeroth, cars, nodeLoad):
     #find optimal route for each node to node dest
-    nextNode = np.zeros(shape=58)
+    global nextNode
     for i in range(len(nextNode)):
         if i == 51:
             continue
-        nextNode[i] = Dijkst(i,51,wei)[1]
-    #move 70% caars accordingly, rest stay <- calc 0.7 of num. floor. do cars-num, add num to optimal node
-    #print "et", nextNode[40]
+        nextNode[i] = int(Dijkst(i,51,wei)[1])
+
+    #Node 52. 60% of cars stay.
+    carsAt51 = cars[51]
+    nodeLoad[51] = max(nodeLoad[51], carsAt51)
+    cars[51] = math.floor(0.6 * carsAt51)
+
     copyCars = np.copy(cars)
     for i in RANGEEX51:
         carsAtNode = copyCars[i]
         nodeLoad[i] = max(nodeLoad[i], carsAtNode)
         oldCars = cars[i]
-        cars[i] = math.floor(oldCars - 0.7 * carsAtNode)
-        carsMoved = oldCars - cars[i]
+        carsLeft = math.floor(oldCars - 0.7 * carsAtNode)
+        cars[i] = carsLeft
+        carsMoved = oldCars - carsLeft
         cars[int(nextNode[i])] += carsMoved
-
-    #Node 52. 60% of cars stay.
-    cars[51] = math.floor(0.6 * cars[51])
-    nodeLoad[51] = max(nodeLoad[51], cars[51])
+    
     #update wij
-    oldwij = np.copy(wei)
     updateWij(RomeA, RomeB, wei, weiZeroth, cars)
-    from functools import reduce
-    product = (oldwij == wei).all()
-    print "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", product 
     return
 
+def blockNode30(weiZeroth):
+    weiZeroth[29, 25] = 9999
+    weiZeroth[29, 34] = 9999
+    weiZeroth[29, 42] = 9999
+    weiZeroth[29, 44] = 9999
+    weiZeroth[29, 20] = 9999
+
 if __name__ == "__main__":
+    RomeX = np.empty(0,dtype=float)
+    RomeY = np.empty(0,dtype=float)
+    with open('RomeVertices','r') as file:
+        AAA = csv.reader(file)
+        for row in AAA:
+            RomeX = np.concatenate((RomeX,[float(row[1])]))
+            RomeY = np.concatenate((RomeY,[float(row[2])]))
+    file.close()
+
     RomeA = np.empty(0,dtype=int)
     RomeB = np.empty(0,dtype=int)
     RomeV = np.empty(0,dtype=float)
@@ -88,11 +103,14 @@ if __name__ == "__main__":
     destination = 51 # Coliseum
     cars = np.zeros(shape=58)
     nodeLoad = np.zeros(shape=58)
-    wei = fillWei(RomeA, RomeB, RomeV)
+    #wei = fillWei(RomeA, RomeB, RomeV)
+    wei = calcWei(RomeX, RomeY, RomeA, RomeB, RomeV)
     weiZeroth = np.copy(wei)
+
+    #blockNode30(weiZeroth)
+
     print "Simulating Rome #Roger\nInitial state:"
     printCars(cars)
-    #print wei
     for i in range(200):
         if i < 180:
             cars[injectionPoint] += 20
@@ -101,11 +119,10 @@ if __name__ == "__main__":
         RomeSimulate(RomeA, RomeB, wei, weiZeroth, cars, nodeLoad)
         count = 0
         nodes = []
-        for j in range(len(cars)):
-            if cars[j] != 0:
-                count += 1
-                nodes.append(j)
-        print "nodes in use", count, nodes
+        #for j in range(len(cars)):
+        #    if cars[j] != 0:
+        #        count += 1
+        #        nodes.append(j)
+        #print "nodes in use", count, nodes
         printCars(cars)
-        print wei
     printCars(nodeLoad)
